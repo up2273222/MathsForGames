984a97ca546b7a514ba35b84bf087f67180d8f71
up2273222 <up2273222@myport.ac.uk>
2026-02-12 11:37:50 +0000

    Week 2

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,16 +1,26 @@
+using System.Numerics;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
 
 public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       print(MathCore.AddVector(new Vector2(2, 5),new Vector2(3, 0)));
-       print(MathCore.SubtractVector(new Vector2(2, 5), new Vector2(5, 3)));
-       print(MathCore.VectorLen(new Vector2(3,4)));
-       print(MathCore.VectorDist(new Vector2(1,1), new Vector2(4,5)));
+        Vector2 enemypos = new Vector2(5,10);
         
+        Vector2 playerpos = new Vector2(5,7);
         
+        Vector2 enemyfacing = new Vector2(0,1);
+        
+        enemypos = Vector2.Normalize(enemypos);
+        playerpos = Vector2.Normalize(playerpos);
+        enemyfacing = Vector2.Normalize(enemyfacing);
+        
+        print(Vector2.Dot(playerpos-enemypos,enemyfacing));
+        print(Vector2.Dot(enemypos-playerpos, enemyfacing));
+        
+
     }
 
     
